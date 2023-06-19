d29e8dbdc77e77ac16d84c926d59dd215525eb65
SHEHEER ALI <nssheheerali@gmail.com>
2023-06-19 16:32:55 +0530

    Initial Commit

---
+++ Auth/Response.cs
@@ -0,0 +1,8 @@
+namespace classromm.Auth
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ Auth/UserRoles.cs
@@ -0,0 +1,8 @@
+namespace classromm.Auth
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
